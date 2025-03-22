7bbf13defe499d1fddd4490e0527b56de1e79e34
NStoreML <afisikna6@gmail.com>
2025-03-22 23:19:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+///////////////////////////
+// Aayu5h and Sahil
+// https://discord.gg/uepgJzsf6n
+// https://spicydevs.me/
+///////////////////////////
+
+module.exports = {
+  // refer to tutorial1.png and tutorial2.png in the repo for help
+  showTime: false, // Toggle to show or hide time in status (true/false)
+  token: "" || process.env.TOKEN,
+  timeZone: "Asia/Jakarta", //Your Timezone, eg Asia/Kolkata
+  Name: "N Store!",
+  State: "Discord Shop",
+  Details: "N Store!",
+  FirstButtonName: "Server!",
+  FirstButtonUrl: "https://discord.gg/CPc8ReEgVu",
+  SecondButtonName: "Whatsapp!",
+  SecondButtonUrl: "https://wa.me/6285186834494",
+  LargeImage: "https://cdn.discordapp.com/attachments/1336707925401534593/1340824667505823844/20250208_003822.jpg?ex=67b3c3c8&is=67b27248&hm=b2093d17cec56b93a59536a0c34075b36741e4563206dfceab03521c396cc67f&", // DISCORD CDN IMAGE ONLY
+  LargeText: "Topup Games & Nitro DC!", // hover text for large image
+  SmallImage: "https://cdn.discordapp.com/attachments/1336707925401534593/1340825188069150760/verified-verified-twitter.gif?ex=67b3c444&is=67b272c4&hm=d488229d1a89f49196b950a9db4f932ec5cbb8997b7d73dd138e57546dd1022d&", // DISCORD CDN IMAGE ONLY
+  SmallText: "Verified!", // hover text for small image
+};
+
+///////////////////////////
+// Aayu5h and Sahil
+// https://discord.gg/uepgJzsf6n
+// https://spicydevs.me/
+///////////////////////////
